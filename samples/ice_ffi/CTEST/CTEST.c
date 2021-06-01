5bdd92606a7071917ed17f3553d11a9a9f35e92a
Rabia Alhaffar <steria773@gmail.com>
2021-06-01 16:49:54 +0300

    Rewrote CTEST.c to support ANSI C standards!

--- samples/ice_ffi/CTEST/CTEST.c
+++ samples/ice_ffi/CTEST/CTEST.c
@@ -1,28 +1,24 @@
-// NOTE: Compile this as CTEST anyway you would like (with the shared lib extension)
-// On Windows:  CTEST.dll
-// On OSX:      CTEST.dylib
-// Elsewhere:   CTEST.so
-#include <stdio.h>
-
-#if defined(__WIN) || defined(_WIN32_) || defined(_WIN64_) || defined(WIN32) || defined(__WIN32__) || defined(WIN64) || defined(__WIN64__) || defined(WINDOWS) || defined(_WINDOWS) || defined(__WINDOWS) || defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__) || defined(_MSC_VER) || defined(__WINDOWS__) || defined(_X360) || defined(XBOX360) || defined(__X360) || defined(__X360__) || defined(_XBOXONE) || defined(XBONE) || defined(XBOX) || defined(__XBOX__) || defined(__XBOX) || defined(__xbox__) || defined(__xbox) || defined(_XBOX) || defined(xbox)
+// NOTE: Compile this as shared library by the way you do (with the shared lib extension)
+// Microsoft Platforms:  CTEST.dll
+// Apple Platforms:      CTEST.dylib
+// Elsewhere:            CTEST.so
+#if defined(__WIN) || defined(_WIN32_) || defined(_WIN64_) || defined(WIN32) || defined(__WIN32__) || defined(WIN64) || defined(__WIN64__) || defined(WINDOWS) || defined(_WINDOWS) || defined(__WINDOWS) || defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__) || defined(_MSC_VER) || defined(__WINDOWS__) || defined(_X360) || defined(XBOX360) || defined(__X360) || defined(__X360__) || defined(_XBOXONE) || defined(XBONE) || defined(XBOX) || defined(__XBOX__) || defined(__XBOX) || defined(__xbox__) || defined(__xbox) || defined(_XBOX) || defined(xbox) || ((defined(_XBOX_ONE) || defined(_DURANGO)) && defined(_TITLE))
 #  define PLATFORM_MICROSOFT
 #endif
 
 #if defined(PLATFORM_MICROSOFT)
-#  define DLLEXPORT __declspec(dllexport)
-#  define DLLIMPORT __declspec(dllimport)
+#  define CAPI extern __declspec(dllexport)
 #else
-#  define DLLEXPORT
-#  define DLLIMPORT
+#  define CAPI extern
 #endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-DLLEXPORT inline int test(void);
+CAPI int test(void);
 
-DLLEXPORT inline int test(void) {
+CAPI int test(void) {
     return 42;
 }
 
