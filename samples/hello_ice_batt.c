b7e0a4521185d6c7f762e0c6da7f20dde3d51fa9
Rabia Alhaffar <steria773@gmail.com>
2021-12-30 17:11:29 +0200

    Added ice_batt.h example!

---
+++ samples/hello_ice_batt.c
@@ -0,0 +1,30 @@
+/* Define the implementation of the library then include it */
+#define ICE_BATT_IMPL
+#include "ice_batt.h"
+
+#include <stdio.h>
+
+/* Helper */
+#define trace(fname, str) printf("[%s : line %d] %s() => %s\n", __FILE__, __LINE__, fname, str);
+
+int main(int argc, char **argv) {
+    /* Struct that contains information about the battery */
+    ice_batt_info info;
+
+    /* Fetch battery information and store the information in the struct */
+    ice_batt_error err = ice_batt_get_status(&info);
+
+    /* If the function failed to fetch battery information, Trace error then terminate the program */
+    if (err != ICE_BATT_ERROR_OK) {
+        trace("ice_batt_get_status", "ERROR: failed to fetch battery information!");
+        return -1;
+    }
+
+    /* Print information we got */
+    printf("Device has battery: %s\nIs battery charging: %s\nBattery Level: %u\n",
+      (info.exists == ICE_BATT_TRUE) ? "YES" : "NO",
+      (info.charging == ICE_BATT_TRUE) ? "YES" : "NO",
+      info.level);
+
+    return 0;
+}
